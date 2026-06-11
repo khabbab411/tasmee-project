2dc4eb45c38272967dcf775df3d1f1ac32344de2
khabbab411 <khabbab411@gmail.com>
2026-06-11 21:51:48 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,17 @@
+from flask import Flask
+from threading import Thread
+import os
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "✅ البوت شغال"
+
+def run():
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
