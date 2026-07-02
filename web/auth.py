2d9cf8710c49cf635a1c33bcc38b3b952570ae10
khabbab411 <khabbab411@gmail.com>
2026-07-02 22:44:09 +0300

    Update auth.py

--- web/auth.py
+++ web/auth.py
@@ -1,23 +1,24 @@
-import sqlite3
+from database import get_connection
 from werkzeug.security import check_password_hash
-
-DATABASE_NAME = "users.db"
 
 
 def authenticate(username, password):
-    conn = sqlite3.connect(DATABASE_NAME)
-    conn.row_factory = sqlite3.Row
+    conn = get_connection()
+    cur = conn.cursor()
 
-    teacher = conn.execute(
+    cur.execute(
         """
         SELECT *
         FROM teachers
-        WHERE username = ?
-        AND is_active = 1
+        WHERE username = %s
+        AND is_active = TRUE
         """,
         (username,)
-    ).fetchone()
+    )
 
+    teacher = cur.fetchone()
+
+    cur.close()
     conn.close()
 
     if teacher is None:
